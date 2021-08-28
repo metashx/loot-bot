a813fca2d34496a13754b495c968b27895ddd942
Kaan Tasbas <mktasbas@gmail.com>
2021-08-27 22:27:33 -0400

    Update id and rarity files. Change msg order

--- main.py
+++ main.py
@@ -14,21 +14,26 @@ client = discord.Client()
 async def on_message(message):
     if lootIdPattern.search(message.content):
         lootId = int(message.content[1:])
-        if lootId > 8000:
+        if lootId < 1 or lootId > 8000:
             return
         
         # Get dict for this bag
-        bag = loot[lootId][f'{lootId}']
+        bag = loot[lootId-1][f'{lootId}']
+
+        # Turn bag values into list, and order to match sales bot
+        bagItems = [ 
+            bag['weapon'], bag['chest'], bag['head'], bag['waist'], 
+            bag['foot'], bag['hand'], bag['neck'], bag['ring']
+        ]
 
         # Get rarity for this bag
         rareId = list(filter(lambda l: l['lootId'] == lootId, rare))[0]
         rareRank = rareId['rarest']
-        rareScore = rareId['score']
         
         msg = discord.Embed(title = f'**Bag #{lootId}**')
         msg.add_field(
             name='-',
-            value=('\n').join(list(bag.values())),
+            value=('\n').join(bagItems),
             inline=True
         )
         msg.set_footer(text=f'Rarity Rank {rareRank}')
